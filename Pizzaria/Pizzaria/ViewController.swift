138273463d8a27f13d1007d8a70d77785240c873
EdgarSocrates98 <edgarsocrates98@gmail.com>
2017-05-24 23:29:53 -0300

    DEMONSTRATIVO DE PIZZARIA(passagem entre paginas)

---
+++ Pizzaria/Pizzaria/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pizzaria
+//
+//  Created by Edgar Sócrates Caria on 22/04/17.
+//  Copyright © 2017 Usuário Convidado. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("RM75736")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
